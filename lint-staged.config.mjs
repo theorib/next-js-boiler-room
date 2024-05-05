d1c513a1a23dfc9760f247d340027fdf36549648
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-05-05 14:59:34 +0100

    chore: remove cache flag from eslint on lint-commit

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 const config = {
-  '**/*.{js,jsx,ts,tsx}': ['eslint --cache --fix'],
+  '**/*.{js,jsx,ts,tsx}': ['eslint --fix'],
   '**/*.{ts,tsx}': () => 'tsc',
   '**/*.{html,json,css,scss,md,mdx,js,jsx,ts,tsx}': ['prettier -w'],
 };
