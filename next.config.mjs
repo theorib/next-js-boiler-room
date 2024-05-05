61705ce03b0816b5905c496d38c9e9fab50ba478
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-05-05 14:09:10 +0100

    chore: remove linting and ts checks from next builds and move them to lint-staged workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // we will handle errors with git action hooks
+  typescript: {
+    ignoreDevErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [220, 390, 430, 640, 768, 1080, 1280, 1920, 2048, 2560, 3840],
