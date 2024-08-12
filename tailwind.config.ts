3ea19a9eaefc4b8888e2942926c4ef40f6f0de12
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-08-12 19:24:53 +0100

    fix(tailwind): change daisyui import from cjs to esm format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss';
-
+import daisyui from 'daisyui';
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -27,6 +27,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
 };
 export default config;
