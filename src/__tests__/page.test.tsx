bc378b292813456274c162d4e1e1e30c6bb1dd73
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-05 11:40:04 +0000

    ✨ feat(app): add counter functionality to homepage
    - implement button to increment counter on homepage
    - refactor homepage layout to improve user interaction
    
    🔧 chore(pkg): update dependencies
    
    - add @radix-ui/react-slot and class-variance-authority
    - ensure compatibility with current project setup
    
    ✅ test(tests): replace temp basic
    
    - migrate from jsdom to vitest browser-mode with playwright for testing
    - add a new page test to improve test coverage
    
    💄 style(button): create reusable button component
    
    - implement button component using Radix UI and CVA
    - ensure button adheres to consistent styling guidelines

---
+++ src/__tests__/page.test.tsx
@@ -0,0 +1,39 @@
+import { expect, test, describe } from 'vitest';
+import { page, userEvent } from '@vitest/browser/context';
+import { render } from 'vitest-browser-react';
+
+import HomePage from '@/app/page';
+import { Suspense } from 'react';
+
+describe('Temp Test', () => {
+  test('Home Page Temp', async () => {
+    const screen = render(
+      <Suspense>
+        <HomePage />
+      </Suspense>,
+    );
+    const heading = screen.getByRole('heading', {
+      name: 'I am the home page',
+    });
+    const img = screen.getByRole('img', {
+      name: 'land of the wind',
+    });
+    await expect.element(heading).toBeVisible();
+    await expect.element(img).toBeVisible();
+
+    const btn = page.getByRole('button', { name: 'Click Me' });
+    const counter = page.getByText('Counter: 0');
+    await expect.element(btn).toBeVisible();
+    await expect.element(counter).toBeVisible();
+
+    await userEvent.click(btn);
+
+    await expect.element(page.getByText('Counter: 1')).toBeInTheDocument();
+
+    await userEvent.click(btn);
+
+    await expect
+      .element(page.getByText('Counter: 2'))
+      .toHaveTextContent('Counter: 2');
+  });
+});
