c156dd2fabf09756e149a7f8550e1bc5f063fa38
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-07 17:33:36 +0000

    💄 style(ui): update sidebar cookie name for clarity
    - change SIDEBAR_COOKIE_NAME to 'sidebar_is_open_state' for better readability

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -26,7 +26,7 @@ import {
 } from '@/components/ui/tooltip'
 import { useSetCookie } from 'cookies-next/client'
 
-export const SIDEBAR_COOKIE_NAME = 'sidebar_state'
+export const SIDEBAR_COOKIE_NAME = 'sidebar_is_open_state'
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
 const SIDEBAR_WIDTH = '16rem'
 const SIDEBAR_WIDTH_MOBILE = '18rem'
