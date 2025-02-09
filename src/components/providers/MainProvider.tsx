fc1cb55b01424fec2101c6096830896517a94933
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-09 00:27:44 +0000

    💄 style(MainProvider): adjust import order and cleanup comments
    - reorder imports for better readability
    - remove commented-out code for clarity

--- src/components/providers/MainProvider.tsx
+++ src/components/providers/MainProvider.tsx
@@ -1,12 +1,10 @@
 import { ThemeProvider } from '@/components/providers/theme-provider'
-import { PropsWithChildren } from 'react'
 import { getCookie } from 'cookies-next/server'
 import { cookies } from 'next/headers'
 import { SIDEBAR_COOKIE_NAME, SidebarProvider } from '@/components/ui/sidebar'
+import { type PropsWithChildren } from 'react'
 
 export default async function MainProvider({ children }: PropsWithChildren) {
-  // const cookieStore = await cookies()
-  // const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true'
   const defaultOpen =
     (await getCookie(SIDEBAR_COOKIE_NAME, { cookies })) === 'true'
 
