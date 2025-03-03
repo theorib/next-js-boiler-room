ba142c3dcf7e3379af2fbcaa25dcb3fbf2c6cdad
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-03-03 12:05:32 +0000

    ✨ feat(nextconfig): optimize lucide-react package imports
    - add experimental optimizePackageImports option for lucide-react to improve performance

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   // we will handle errors with git action hooks
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
